c4d84d30691ac603695b15c42a841b40296db722
da8ma <ryo.8mada1020@gmail.com>
2019-09-15 16:54:54 +0900

    add: settings and file for fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,15 @@
-## PATH
+## fisher auto installation
+## https://github.com/jorgebucaran/fisher#bootstrap-installation
 
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+## PATH
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+set -g fish_prompt_pwd_dir_length 0
+
