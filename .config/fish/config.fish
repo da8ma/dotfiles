2af8c9f4530b27f70fdbbbb8d98835d8b3961201
da8ma <ryo.8mada1020@gmail.com>
2019-11-09 21:52:33 +0900

    set enviroment variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,11 +7,14 @@ if not functions -q fisher
     fish -c fisher
 end
 
-#  variables
+# variables
+set -x LC_ALL en_US.UTF-8
 set -x PIPENV_VENV_IN_PROJECT 1
+
 
 # PATH
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 set -g fish_prompt_pwd_dir_length 0
+
