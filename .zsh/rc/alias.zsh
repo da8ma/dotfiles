e2685e8660d7038f9e4418358fef964d35c9ec0e
da8ma <ryo.8mada1020@gmail.com>
2019-12-04 12:51:44 +0900

    create zsh config dir and add alias file

---
+++ .zsh/rc/alias.zsh
@@ -0,0 +1,12 @@
+# --------------------------------------------------
+#  Aliases
+# --------------------------------------------------
+
+alias gip='curl ipinfo.io/ip'
+alias getpass='openssl rand -base64 10'
+alias check='brew upgrade'
+alias d='docker'
+alias dc='docker-compose'
+alias drmi='docker rmi'
+alias be='bundle exec'
+
